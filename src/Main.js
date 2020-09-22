3056570013b2d1b946ac1392df75906999ae07f4
OmegaRogue <thatomegarogue@gmail.com>
2020-09-23 00:26:46 +0200

    fix router to actually work again

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect,  } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Omega from './pages/Omega';
@@ -8,14 +8,16 @@ import Others from './pages/Others';
 import Projects from './pages/Projects';
 
 const Main = () => {
+
     return (
         <Switch> {/* The Switch decides which component to show based on the current URL.*/}
             <Route exact path='/' component={Home}/>
-            <Route exact path='/#' component={Home}/>
-            <Route exact path='/#/omega' component={Omega}/>
-            <Route exact path='/#/omegarogue' component={OmegaRogue}/>
-            <Route exact path='/#/others' component={Others}/>
-            <Route exact path='/#/projects' component={Projects}/>
+            <Route exact path='/omega' component={Omega}/>
+            <Route exact path='/omegarogue' component={OmegaRogue}/>
+            <Route exact path='/others' component={Others}/>
+            <Route exact path='/projects' component={Projects}/>
+            <Route component={Home} />
+
         </Switch>
     );
 }
