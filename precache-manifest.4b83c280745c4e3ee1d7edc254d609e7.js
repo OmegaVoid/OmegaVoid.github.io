06bd3e8d26c60c5967ba12145b35a36ec302693b
OmegaRogue <thatomegarogue@gmail.com>
2020-09-22 23:50:43 +0200

    Updates

---
+++ precache-manifest.4b83c280745c4e3ee1d7edc254d609e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8312fcaaa54cd3e71dd8c63796ddb6e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8b181bc2cc515683a95",
+    "url": "/static/css/main.13580c5b.chunk.css"
+  },
+  {
+    "revision": "49750168e899bd0017e6",
+    "url": "/static/js/2.041d4416.chunk.js"
+  },
+  {
+    "revision": "fbe06bcc249d3af1b96e1e74b0ad1f88",
+    "url": "/static/js/2.041d4416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b181bc2cc515683a95",
+    "url": "/static/js/main.d2e5d9c0.chunk.js"
+  },
+  {
+    "revision": "b3faf9ba9117eedacccd",
+    "url": "/static/js/runtime-main.0f50b45e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
